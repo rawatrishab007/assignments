612b43f28407368289fdfa8c98feba1c26ab895b
rawatrishab007 <rawatrishab007@gmail.com>
2026-08-04 22:30:09 +0530

    questions done

--- assign1/src/class5/Tdo.jsx
+++ assign1/src/class5/Tdo.jsx
@@ -10,26 +10,39 @@ const Tdo = () => {
         if(action.type=="inp"){
             return {
                 ...state,
-                input:action.payload
+                input:action.payload,
+                
+                
+                
             }
         }else if(action.type=="add"){
+            return
+                ...state,
+                list:list.state[...state.list,state.input],
+                
+            }
+        }else if(action.type=="del"){
             return{
                 ...state,
-                list:[...state.list,state.input]
+                list:state.list.filter((_,id)=>id!=action.payload)
             }
         }
+       
 
     }
     let [state,dispatch]=useReducer(reducer,data)
   return (
     <div>
+        <h1> Todo list </h1>
       <input onChange={(e)=>dispatch({type:"inp",payload:e.target.value})}></input>
       <button onClick={()=>dispatch({type:"add"})}>add</button>
+      <button onClick={()=>dispatch({type:"del",payload:idx})}>Delete</button>
       {
         state.list.map((a)=>{
             return(
                 <>
                 <li>{a}</li>
+                
                 </>
             )
         })
